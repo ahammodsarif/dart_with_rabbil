c5531be4df58602f2ece490d71e7b2ffbb9d7580
Ahammod Sarif <ahammodtannababu@gmail.com>
2024-08-16 00:33:56 +0600

    map update

---
+++ bin/map.dart
@@ -0,0 +1,12 @@
+void main(){
+  var students = {
+    'Name':'Tanna',
+    'Age':25,
+    'Branch':'Conputer Science'
+  };
+
+  print(students.hashCode);
+  students['Country']='Bangladesh';
+
+  print(students);
+}
