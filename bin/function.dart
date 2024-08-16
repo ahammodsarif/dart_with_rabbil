e3b6148d35467877080d0e7b6a75288bde531030
Ahammod Sarif <ahammodtannababu@gmail.com>
2024-08-16 08:42:48 +0600

    function

---
+++ bin/function.dart
@@ -0,0 +1,13 @@
+addTwoNumber(int x, double y){
+    return x+y;
+  }
+
+  void main(){
+    var result = addTwoNumber(5, 45.45) + 25;
+    var result1 = addTwoNumber(10, 50.50) + 30;
+    var result2 = addTwoNumber(15, 55.55) + 35;
+
+    print(result);
+    print(result1);
+    print(result2);
+  }
