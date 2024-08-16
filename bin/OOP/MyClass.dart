107c7f8749aaebc37ab284394965f4e88471bd29
Ahammod Sarif <ahammodtannababu@gmail.com>
2024-08-16 11:50:10 +0600

    class

---
+++ bin/OOP/MyClass.dart
@@ -0,0 +1,11 @@
+class MyClass{
+  var MyName = 'Tanna';
+  var Alphabet = ['A','B','C'];
+
+  addTwoNumber(int x,int y){
+    print(x+y);
+  }
+  addThreeNumber(int x, int y, int z){
+    print(x+y+z);
+  }
+}
---
+++ bin/OOP/class.dart
@@ -0,0 +1,10 @@
+import 'MyClass.dart';
+void main(){
+  var obj = new MyClass();
+  obj.addTwoNumber(20, 30);
+  obj.addThreeNumber(20, 30, 40);
+
+  print(obj.MyName);
+  print(obj.Alphabet[0]);
+
+}
