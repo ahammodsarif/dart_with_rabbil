38c4f20c020d8b3826a69b948c130ba0a6276c2c
Ahammod Sarif <ahammodtannababu@gmail.com>
2024-08-16 18:06:51 +0600

    update class function

--- bin/OOP/MyClass.dart
+++ bin/OOP/MyClass.dart
@@ -1,11 +1,35 @@
+// class MyClass{
+//   var MyName = 'Tanna';
+//   var Alphabet = ['A','B','C'];
+//
+//   addTwoNumber(int x,int y){
+//     print(x+y);
+//   }
+//   addThreeNumber(int x, int y, int z){
+//     print(x+y+z);
+//   }
+//   addFourNumber(int a, int b, int c, int d){
+//     print(a+b+c+d);
+//   }
+// }
+
 class MyClass{
   var MyName = 'Tanna';
-  var Alphabet = ['A','B','C'];
+  var Alphabet = ['A','B','C','D','E'];
 
-  addTwoNumber(int x,int y){
+  addTwoNumber(int x, int y){
     print(x+y);
   }
+
   addThreeNumber(int x, int y, int z){
     print(x+y+z);
   }
+
+  addFourNumber(int a, int b, int c, int d){
+    print(a+b+c+d);
+}
+
+addFiveNumber(int a, int b, int c, int d, int e){
+    print(a+b+c+d+e);
+}
 }
--- bin/OOP/class.dart
+++ bin/OOP/class.dart
@@ -1,10 +1,23 @@
+// import 'MyClass.dart';
+// void main(){
+//   var obj = new MyClass();
+//   obj.addTwoNumber(20, 30);
+//   obj.addThreeNumber(20, 30, 40);
+//   obj.addFourNumber(20, 10, 30, 40);
+//
+//   print(obj.MyName);
+//   print(obj.Alphabet);
+//
+// }
+
 import 'MyClass.dart';
 void main(){
   var obj = new MyClass();
-  obj.addTwoNumber(20, 30);
-  obj.addThreeNumber(20, 30, 40);
+  obj.addTwoNumber(20,30);
+  obj.addThreeNumber(20,30,40);
+  obj.addFourNumber(20,30,40,50);
+  obj.addFiveNumber(20,30,40,50,60);
 
   print(obj.MyName);
-  print(obj.Alphabet[0]);
-
+  print(obj.Alphabet);
 }
