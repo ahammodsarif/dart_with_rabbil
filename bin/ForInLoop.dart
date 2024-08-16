c986aca4b5ddcba3f95a6443cccc9e326686ba2c
Ahammod Sarif <ahammodtannababu@gmail.com>
2024-08-16 08:24:55 +0600

    ForInLoop

---
+++ bin/ForInLoop.dart
@@ -0,0 +1,7 @@
+void main(){
+  var AlphabetList=['A','B','C','D','E','F'];
+
+  for(var OneAlphabet in AlphabetList){
+    print(OneAlphabet);
+  }
+}
---
+++ bin/map2.dart
@@ -0,0 +1,11 @@
+void main(){
+  var person = new Map();
+  person['name']='tanna';
+  person['age']=35;
+  person['city']='dhaka';
+
+  person.addAll({'country':'bangladesh','blood':'0+'});
+
+  print(person);
+
+}
